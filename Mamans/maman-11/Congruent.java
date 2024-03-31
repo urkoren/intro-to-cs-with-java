171bda011131a11221885306b0dcaeccbc490de5
Ur Koren <ork14790@gmail.com>
2024-03-31 13:10:26 +0300

    Update Congruent.java

--- Mamans/maman-11/Congruent.java
+++ Mamans/maman-11/Congruent.java
@@ -33,35 +33,21 @@ public class Congruent
         double x23 = scan.nextDouble();
         double y23 = scan.nextDouble();
 
-        double distance_1a = Math.sqrt(Math.pow((x12 - x11), 2) + Math.pow((y12 - y11), 2));
-        double distance_1b = Math.sqrt(Math.pow((x13 - x12), 2) + Math.pow((y13 - y12), 2));
-        double distance_1c = Math.sqrt(Math.pow((x11 - x13), 2) + Math.pow((y11 - y13), 2));
+        double a1 = Math.sqrt(Math.pow((x12 - x11), 2) + Math.pow((y12 - y11), 2));
+        double b1 = Math.sqrt(Math.pow((x13 - x12), 2) + Math.pow((y13 - y12), 2));
+        double c1 = Math.sqrt(Math.pow((x11 - x13), 2) + Math.pow((y11 - y13), 2));
 
-        double distance_2a = Math.sqrt(Math.pow((x22 - x21), 2) + Math.pow((y22 - y21), 2));
-        double distance_2b = Math.sqrt(Math.pow((x23 - x22), 2) + Math.pow((y23 - y22), 2));
-        double distance_2c = Math.sqrt(Math.pow((x21 - x23), 2) + Math.pow((y21 - y23), 2));
+        double a2 = Math.sqrt(Math.pow((x22 - x21), 2) + Math.pow((y22 - y21), 2));
+        double b2 = Math.sqrt(Math.pow((x23 - x22), 2) + Math.pow((y23 - y22), 2));
+        double c2 = Math.sqrt(Math.pow((x21 - x23), 2) + Math.pow((y21 - y23), 2));
 
-        // System.out.println ("The first triangle is (" + x11+","+y11+") ("+ x12+","+y12+") (" + x11+","+y11+").");
-        // System.out.println ("Its lengths are " + distance_1a +", " + distance_1b + ", " + distance_1c + ".");
-        
-        // System.out.println ("The first triangle is (" + x21+","+y21+") ("+ x22+","+y22+") (" + x21+","+y21+").");
-        // System.out.println ("Its lengths are " + distance_2a +", " + distance_2b + ", " + distance_2c + ".");
-
-        double total_distance_2 = distance_2a + distance_2b + distance_2c;
-
-        if (distance_1a == distance_2a || distance_1a == distance_2b || distance_1a == distance_2c){
-            total_distance_2 -= distance_1a;
-            if (distance_1b == distance_2a || distance_1b == distance_2b || distance_1b == distance_2c){
-                total_distance_2 -= distance_1b;
-                if (distance_1c == distance_2a || distance_1c == distance_2b || distance_1c == distance_2c){
-                    total_distance_2 -= distance_1c;
-                    if (total_distance_2 == 0){
-                        System.out.println("The triangles are congruent");
-                        return;
-                    }
-                }
-            }
-        }
-        System.out.println("The triangles are not congruent");
+        if ((a1 == a2) && ((b1 == b2) && (c1 == c2) || ((b1 == c2) && (c1 == b2))))
+            System.out.println("The triangles are congruent");
+        else if ((a1 == b2) && ((b1 == a2) && (c1 == c2) || ((b1 == c2) && (c1 == a2))))
+            System.out.println("The triangles are congruent");
+        else if ((a1 == c2) && ((b1 == a2) && (c1 == b2) || ((b1 == b2) && (c1 == a2))))
+            System.out.println("The triangles are congruent");
+        else
+            System.out.println("The triangles are not congruent");
     }
 }
